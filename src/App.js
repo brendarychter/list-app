4e847ca98f9b4e22ee243fbaedb282326996ebba
brenda.rychter <brychter@despegar.com>
2021-04-11 17:20:15 -0300

    table with react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Table } from 'react-bootstrap';
 
 export default function App() {
   return (
-    <div className="app-container">
-      Listado
+    <div className="app-container container mt-3">
+      <h1>Listado de activos</h1>
+      <Router>
+        <Switch>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Partida</th>
+                <th>Descripción</th>
+                <th>Aplicabilidad</th>
+                <th>Cumplimiento controles</th>
+                <th>Acción</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr></tr>
+            </tbody>
+          </Table>
+          {/*  component={Dashboard}*/}
+          <Route path="/dashboard" />
+        </Switch>
+      </Router>
     </div>
   );
 }
-
