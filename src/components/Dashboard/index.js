2c7ee4dab50b3bbe622b75afaab11cc5099b1fb5
brenda.rychter <brychter@despegar.com>
2021-05-03 13:19:20 -0300

    main - use material-table

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,34 +2,65 @@ import React, { useState, useEffect } from 'react';
 import db from '../.././utils/db.json';
 import schema from '../.././utils/schema';
 import Table from './Table';
+import MaterialTable from 'material-table';
 
 export default function Dashboard() {
-  const [data, setData] = useState(null);
-  // const columns = [
-  //     {title:"Partida", field:"partida"},
-  //     {title:"Descripción", field:"description"},
-  //     {title:"Aplicabilidad", field:"aplicabilidad"},
-  //     {title:"Cumplimiento", field:"cumplimiento"},
-  //     {title:"Acción", field:"action"},
-  // ]
+  // const [data, setData] = useState(null);
+  // // const columns = [
+  // //     {title:"Partida", field:"partida"},
+  // //     {title:"Descripción", field:"description"},
+  // //     {title:"Aplicabilidad", field:"aplicabilidad"},
+  // //     {title:"Cumplimiento", field:"cumplimiento"},
+  // //     {title:"Acción", field:"action"},
+  // // ]
+  //
+  // useEffect(() => {
+  //   // fetch("https://jsonplaceholder.typicode.com/users")
+  //   //     .then( res =>res.json() )
+  //   //     .then(res => setData(res))
+  //   new Promise((resolve) => {
+  //     setTimeout(() => {
+  //       resolve(db);
+  //     }, 2000);
+  //   }).then((result) => {
+  //     setData(result);
+  //   });
+  // });
+  //
+  // return (
+  //   <div className="app-container container mt-5">
+  //     <h2 className="mb-4">Listado de activos para: </h2>
+  //     <Table headers={Object.keys(schema)} rows={data} />
+  //   </div>
+  // );
 
+  const [data, setData] = useState([])
+  const columns = [
+    { title: "Partida", field: "partida" },
+    { title: "Descripción", field: "description" },
+    { title: "Aplicabilidad", field: "aplicabilidad" },
+    { title: "Cumplimiento", field: "cumplimiento" },
+    { title: "Acción", field: 'action' }
+  ]
   useEffect(() => {
-    // fetch("https://jsonplaceholder.typicode.com/users")
-    //     .then( res =>res.json() )
-    //     .then(res => setData(res))
-    new Promise((resolve) => {
-      setTimeout(() => {
-        resolve(db);
-      }, 2000);
-    }).then((result) => {
-      setData(result);
-    });
-  });
+    fetch(db)
+        .then(resp => resp.json())
+        .then(resp => {
+          setData(resp)
+        })
+  }, [])
 
   return (
-    <div className="app-container container mt-5">
-      <h2 className="mb-4">Listado de activos para: </h2>
-      <Table headers={Object.keys(schema)} rows={data} />
-    </div>
+      <div className="app-container container mt-5">
+        <h2 className="mb-4">Listado de activos para: </h2>
+        <MaterialTable
+            title="Nombre"
+            data={data}
+            columns={columns}
+            options={{
+                search: false
+            }}
+        />
+      </div>
   );
 }
